7b15a3a8944b5e8caa761032942fb8220bfa7923
Gabriel S. Ferreira <gs_ferreira@id.uff.br>
2024-03-31 20:03:32 -0300

    347

--- leet_code/arrays_and_hashing/347.py
+++ leet_code/arrays_and_hashing/347.py
@@ -1,1 +1,27 @@
 #347. Top K Frequent Elements - https://leetcode.com/problems/top-k-frequent-elements/description/
+
+from typing import List
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        frequent_items = {}
+
+        for n in nums:
+            frequent_items[n] = frequent_items.get(n, 0) + 1
+
+        frequency = sorted(frequent_items.values(), reverse=True)
+        frequency = frequency[:k]
+        k_items = []
+        for key, v in frequent_items.items():
+            if v in frequency:
+                k_items.append(key)
+            
+            if len(k_items) == k:
+                break
+        
+        return k_items
+
+
+print(Solution().topKFrequent([1,1,1,2,2,3], 2))
+print(Solution().topKFrequent([1], 1))
+        
