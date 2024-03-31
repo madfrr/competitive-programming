13df4732a351ee7d4e2382d535889fa1a74e3530
Gabriel S. Ferreira <gs_ferreira@id.uff.br>
2024-03-31 20:48:29 -0300

    1

---
+++ leet_code/arrays_and_hashing/1.py
@@ -0,0 +1,32 @@
+#1. Two Sum - https://leetcode.com/problems/two-sum/description/ 
+# Tempo - 20h42
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        i, j = 0, 1
+        for i in range(0, len(nums) -1):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return None
+
+print(Solution().twoSum([2,7,11,15], 9))
+print(Solution().twoSum([3,2,4], 6))
+print(Solution().twoSum([3,3], 6))
+
+'''
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+'''
